a4991563ef0d59743b5135f98563649678f8e6d7
ByteForge <46260031+ByteForge786@users.noreply.github.com>
2024-09-30 14:53:36 +0530

    Update toolkit.py

--- toolkit.py
+++ toolkit.py
@@ -2,15 +2,12 @@ from typing import List, Optional, Type, Sequence, Dict, Any, Union, Tuple
 from langchain_core.pydantic_v1 import Field, BaseModel
 from langchain_core.tools import BaseToolkit
 from langchain_community.tools import BaseTool
-from langchain_community.utilities.sql_database import SQLDatabase
 from langchain_core.callbacks import (
     CallbackManagerForToolRun,
 )
-from snowflake.connector import SnowflakeConnection
-from sqlalchemy.engine import Result
 import json
 
-class _InfoSQLDatabaseToolInput(BaseModel):
+class _InfoSnowflakeTableToolInput(BaseModel):
     table_names: str = Field(
         ...,
         description=(
@@ -20,11 +17,11 @@ class _InfoSQLDatabaseToolInput(BaseModel):
     )
 
 class InfoSnowflakeTableTool(BaseTool):
-    name: str = "sql_db_schema"
-    description: str = "Get the schema and sample rows for the specified SQL tables."
-    args_schema: Type[BaseModel] = _InfoSQLDatabaseToolInput
+    name: str = "snowflake_table_info"
+    description: str = "Get the schema and sample rows for the specified Snowflake tables."
+    args_schema: Type[BaseModel] = _InfoSnowflakeTableToolInput
 
-    db: SQLDatabase = Field(exclude=True)
+    snowflake_connection: any = Field(exclude=True)
 
     def _run(
         self,
@@ -34,85 +31,80 @@ class InfoSnowflakeTableTool(BaseTool):
         output_schema = ""
         _table_names = table_names.split(",")
         for t in _table_names:
-            schema = self.db.run(f"DESCRIBE TABLE {t}")
-            output_schema += f"Schema for table {t}: {schema}\n"
+            query = f"DESCRIBE TABLE {t}"
+            cursor = self.snowflake_connection.cursor()
+            result = cursor.execute(query).fetchall()
+            cursor.close()
+            output_schema += f"Schema for table {t}: {result}\n"
         return output_schema
 
-class _QuerySQLCheckerToolInput(BaseModel):
+class _QueryCheckerToolInput(BaseModel):
     query: str = Field(..., description="A detailed SQL query to be checked.")
 
-class QuerySQLCheckerTool(BaseTool):
-    template: str = """
-    {query}
-    Double check the {dialect} query above for common mistakes, including:
-    - Using NOT IN with NULL values
-    - Using UNION when UNION ALL should have been used
-    - Using BETWEEN for exclusive ranges
-    - Data type mismatch in predicates
-    - Properly quoting identifiers
-    - Using the correct number of arguments for functions
-    - Casting to the correct data type
-    - Using the proper columns for joins
-
-    If there are any of the above mistakes, rewrite the query. If there are no mistakes, just reproduce the original query.
-
-    Output the final SQL query only.
-
-    SQL Query: """
-    name: str = "sql_db_query_checker"
+class QueryCheckerTool(BaseTool):
+    name: str = "query_checker"
     description: str = """
     Use this tool to double check if your query is correct before executing it.
-    Always use this tool before executing a query with sql_db_query!
+    Always use this tool before executing a query with query_executor!
     """
-    args_schema: Type[BaseModel] = _QuerySQLCheckerToolInput
+    args_schema: Type[BaseModel] = _QueryCheckerToolInput
 
-    db: SQLDatabase = Field(exclude=True)
+    cortex_function: callable = Field(exclude=True)
 
     def _run(
         self,
         query: str,
         run_manager: Optional[CallbackManagerForToolRun] = None,
     ) -> str:
+        template = """
+        {query}
+        Double check the Snowflake SQL query above for common mistakes, including:
+        - Using NOT IN with NULL values
+        - Using UNION when UNION ALL should have been used
+        - Using BETWEEN for exclusive ranges
+        - Data type mismatch in predicates
+        - Properly quoting identifiers
+        - Using the correct number of arguments for functions
+        - Casting to the correct data type
+        - Using the proper columns for joins
+
+        If there are any of the above mistakes, rewrite the query. If there are no mistakes, just reproduce the original query.
+
+        Output the final SQL query only.
+
+        SQL Query: """
+        
         escaped_query = query.replace('"', '\\"').replace("'", "\\'")
-        prompt = self.template.format(query=escaped_query, dialect=self.db.dialect)
+        prompt = template.format(query=escaped_query)
         messages = [
             {
                 'role': 'user',
                 'content': prompt
             }
         ]
-        cortex_query = f"""
-        SELECT SNOWFLAKE.CORTEX.COMPLETE(
-            'mistral-7b',
-            {json.dumps(messages)},
-            {{
-                'guardrails': true
-            }}
-        );
-        """
-        return self.db.run(cortex_query)
+        return self.cortex_function(json.dumps(messages))
 
-class _QuerySQLDataBaseToolInput(BaseModel):
+class _QueryExecutorToolInput(BaseModel):
     query: str = Field(..., description="A detailed and correct SQL query.")
 
-class QuerySQLDataBaseTool(BaseTool):
-    name: str = "sql_db_query"
+class QueryExecutorTool(BaseTool):
+    name: str = "query_executor"
     description: str = """
-    Execute a SQL query against the database and get back the result and query_id.
+    Execute a SQL query against the Snowflake database and get back the result and query_id.
     If the query is not correct, an error message will be returned.
     If an error is returned, rewrite the query, check the query, and try again.
     """
-    args_schema: Type[BaseModel] = _QuerySQLDataBaseToolInput
+    args_schema: Type[BaseModel] = _QueryExecutorToolInput
 
-    con: SnowflakeConnection = Field(exclude=True)
+    snowflake_connection: any = Field(exclude=True)
 
     def _run(
         self,
         query: str,
         run_manager: Optional[CallbackManagerForToolRun] = None,
-    ) -> Tuple[Union[str, Sequence[Dict[str, Any]], Result], Optional[str]]:
+    ) -> Tuple[Union[str, Sequence[Dict[str, Any]]], Optional[str]]:
         try:
-            cursor = self.con.cursor()
+            cursor = self.snowflake_connection.cursor()
             results = cursor.execute(query).fetchall()
             query_id = cursor.sfqid
             cursor.close()
@@ -121,31 +113,27 @@ class QuerySQLDataBaseTool(BaseTool):
             return f"Error: {e}", None
 
 class AgentToolkit(BaseToolkit):
-    db: SQLDatabase = Field(exclude=True)
-    con: SnowflakeConnection = Field(exclude=True)
-
-    @property
-    def dialect(self) -> str:
-        return self.db.dialect
+    snowflake_connection: any = Field(exclude=True)
+    cortex_function: callable = Field(exclude=True)
 
     class Config:
         arbitrary_types_allowed = True
 
     def get_tools(self) -> List[BaseTool]:
-        info_sql_database_tool = InfoSnowflakeTableTool(
-            db=self.db,
+        info_snowflake_table_tool = InfoSnowflakeTableTool(
+            snowflake_connection=self.snowflake_connection,
             description="Input: comma-separated list of tables. Output: schema and sample rows for those tables."
         )
-        query_sql_database_tool = QuerySQLDataBaseTool(
-            con=self.con,
+        query_executor_tool = QueryExecutorTool(
+            snowflake_connection=self.snowflake_connection,
             description="Input: correct SQL query. Output: result and query_id. If error, rewrite and try again."
         )
-        query_sql_checker_tool = QuerySQLCheckerTool(
-            db=self.db,
-            description="Use this to check your query before executing it with sql_db_query."
+        query_checker_tool = QueryCheckerTool(
+            cortex_function=self.cortex_function,
+            description="Use this to check your query before executing it with query_executor."
         )
         return [
-            query_sql_database_tool,
-            info_sql_database_tool,
-            query_sql_checker_tool,
+            query_executor_tool,
+            info_snowflake_table_tool,
+            query_checker_tool,
         ]
